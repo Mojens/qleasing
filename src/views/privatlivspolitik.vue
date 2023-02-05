557b1b76ef51fbe52b59a3190de6d8aa58a1a585
mojens <moha135h@stud.kea.dk>
2023-02-06 00:03:39 +0100

    har fixet at man nu kan bruge envioremental variabler og sat det op så det virker, nu mangler der bare at tilføje de forskellige sider

--- src/views/privatlivspolitik.vue
+++ src/views/privatlivspolitik.vue
@@ -1,21 +1,38 @@
 <template>
   <div>
-    <p>{{ apiKey }}</p>
+    <div v-if="articleData">
+      <h1>{{ articleData.title }}</h1>
+      <div v-html="articleData.body"></div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   data() {
-    const api = import.meta.env.VUE_APP_READER_API
-    console.log(api)
     return {
-      apiKey: import.meta.env.VUE_APP_READER_API
+      articleData: null
     };
+  },
+  async created() {
+    await this.fetchData();
+  },
+  methods: {
+    async fetchData(){
+      const READER_API = import.meta.env.VITE_APP_READER_API
+      const ARTICLE_URL = import.meta.env.VITE_APP_ARTICLE_URL + "2"
+      const response = await fetch(ARTICLE_URL,{
+        headers:{
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${READER_API}`
+        }
+      });
+      const data = await response.json();
+      this.articleData = data.data;
+    }
   }
 };
 </script>
-
 <style>
 @media (min-width: 1024px) {
   .privatlivspolitik {
