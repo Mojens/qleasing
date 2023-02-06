172dcf7e25f9656df51ad5e212ad38b3892aed13
mojens <moha135h@stud.kea.dk>
2023-02-06 21:38:21 +0100

    forsikringspriser

--- src/views/forsikringsPriser.vue
+++ src/views/forsikringsPriser.vue
@@ -1,52 +1,45 @@
 <template>
-    <table>
-        <thead>
-            <tr>
-                <th>Price Group</th>
-                <th>Age</th>
-                <th>Price Month</th>
-                <th>Excess</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="endpoint in endpoints" :key="endpoint.id">
-                <td>{{ endpoint.price_group }}</td>
-                <td>{{ endpoint.age }}</td>
-                <td>{{ endpoint.price_month }}</td>
-                <td>{{ endpoint.excess }}</td>
-            </tr>
-        </tbody>
-    </table>
-</template>
-
-<script>
-export default {
+    <div>
+      <div v-if="articleData">
+        <h1>{{ articleData.title }}</h1>
+        <div v-html="articleData.body"></div>
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {
     data() {
-        return {
-            endpoints: []
-        };
+      return {
+        articleData: null
+      };
     },
-    mounted() {
-        this.fetchEndpoints();
+    async created() {
+      await this.fetchData();
     },
     methods: {
-        async fetchEndpoints() {
-            try {
-                const READER_API = import.meta.env.VITE_APP_READER_API
-                const INSURANCE_PRICES_URL = import.meta.env.VITE_APP_INSURANCE_PRICES_URL
-                const response = await fetch(INSURANCE_PRICES_URL, {
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${READER_API}`
-                    }
-                });
-                const data = await response.json();
-                this.endpoints = data.data;
-            } catch (error) {
-                console.error(error);
-            }
-        }
+      async fetchData(){
+        const READER_API = import.meta.env.VITE_APP_READER_API
+        const ARTICLE_URL = import.meta.env.VITE_APP_ARTICLE_URL + "9"
+        const response = await fetch(ARTICLE_URL,{
+          headers:{
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${READER_API}`
+          }
+        });
+        const data = await response.json();
+        this.articleData = data.data;
+      }
     }
-};
-</script>
+  };
+  </script>
+  <style>
+  @media (min-width: 1024px) {
+    .privatlivspolitik {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
