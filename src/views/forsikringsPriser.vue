233e50545d99a637e25b714ce614c1ab373b570c
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-02-10 14:04:51 +0100

    Added styling to forsikringsPriser.vue

--- src/views/forsikringsPriser.vue
+++ src/views/forsikringsPriser.vue
@@ -3,10 +3,242 @@ import FooterItem from "../components/FooterItem.vue";
 </script>
 <template>
   <div>
-    <div class="pad-section--l center--all owl--l" v-if="articleData">
-      <h1>{{ articleData.title }}</h1>
+    <div class="container___1BnaH"><span class="wrapper___3jnGF"><span class="mobile-layout___20MjK"><div
+      class="banner___2W4p- small___tJeWB white-blue___2fY5Q"
+      style="background-image:url('https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,e_colorize:,h_480,q_80/v1/Bilabonnement%20Limited%20Edition/508-header.616315_tsewwz.jpg')"><div
+      class="content-banner___vS5KQ"><h1 class="title___1K3fO"></h1><div
+      class="markdown___35KUH description___2z7AW"></div></div></div><div class="spots-list___2DWRP"><h2
+      class="title___2LvNh">Følgende er indeholdt i din forsikringspræmie</h2><h3 class="headline___3bXDI">Forsikringspræmie kort fortalt</h3><div
+      class="spots___1YBZo"><div class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+      src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_1_150x150_jctdcw.png"
+      width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">Stenslag</h4><div
+
+      class="markdown___35KUH description___W9AmU"><p>Gratis reparation af stenslag</p></div></div>
+    </div>
+      <div
+        class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+        src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_2_150x150_yhazyj.png"
+        width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">Førerulykkesforsikring</h4><div
+        class="markdown___35KUH description___W9AmU"><p>Gratis førerulykkesforsikring</p></div></div></div><div
+        class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+        src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_3_150x150_wsap05.png"
+        width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">Afleveringsforsikring</h4><div
+        class="markdown___35KUH description___W9AmU"><p>Gratis afleveringsforsikring selvrisiko kr. 7.500/5.000 for alle skader, dog maksimalt 37.500/25.000 totalt ved aflevering</p></div></div></div><div
+        class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+        src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_4_150x150_yumru6.png"
+        width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">Glasforsikring</h4><div
+        class="markdown___35KUH description___W9AmU"><p>Glasforsikring kan tilkøbes for kr. 59,- pr. måned og dækker skade på bilens glas, f.eks. spejl- og lygteglas eller ruder med en selvrisiko på kr. 1.000</p></div></div></div><div
+        class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+        src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_5_150x150_domzhx.png"
+        width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">Autohjælp</h4><div
+        class="markdown___35KUH description___W9AmU"><p>Autohjælp kan tilkøbes for kr. 49,- pr. måned</p></div></div></div><div
+        class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+        src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_6_150x150_wo3aod.png"
+        width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">PBS</h4><div
+        class="markdown___35KUH description___W9AmU"><p>PBS-gebyr kr. 10,- opkræves sammen med den månedlige forsikringspræmie</p></div></div></div></div></div><span
+      class="new-html-block___1_7NG"><h2 class="center--all pad-header--s">Ansvars- og kaskoforsikring gjort helt enkelt</h2>
+<p style="text-align: center;" v-html="articleData.optional_extra_body"></p>
+</span><div
+      class="content___QPcta"></div><span class="new-html-block___1_7NG"></span><span class="spacer___2n7gY small___RGjLY"></span><div
+      class="content___QPcta"><div class="divider-wrapper___1QLQs large___1alyA main___3_Hz1"><div
+      class="divider___3Bain"></div></div></div>
+     <div class="insurance__container center--all owl--s" v-if="articleData">
+      <h2>Forsikringspriser</h2>
       <div class="directus_table" v-html="articleData.body"></div>
 
+    </div></span>
+
+
+
+      <span class="desktop-layout___3I2bp"><div
+        class="header-carousel___slJT0"><div class="carousel___2FWTA carousel___pF_rz"><div
+        class="slide-wrapper___3j6eE"
+        style="overflow-x:hidden"><div
+        class="react-swipeable-view-container"
+        style="flex-direction: row; transition: all 0s ease 0s; direction: ltr; display: flex; will-change: transform; transform: translate(0%, 0px);"><div
+        aria-hidden="false"
+        data-swipeable="true" style="width:100%;-webkit-flex-shrink:0;flex-shrink:0;overflow:auto"><div
+        class="carousel-item___3uQ8S"><div class="content___VzQF7"><div class="texts___1vhn4 light___2X9Hf"><h3
+        class="headline___sN_Xe"></h3><h2 class="title___3LtIp"> <h1 class="h--1 text___A19Im">{{articleData.title
+        }}</h1><span class="name___33seR"><div class="text___A19Im">Se hvordan</div></span></h2><div
+        class="markdown___35KUH text___A19Im"></div></div><div
+        class="image___15CNk use-image-as-background___1efWp"><img
+        alt=""
+        src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fill,q_80,w_1280/v1/Bilabonnement%20Limited%20Edition/508-header.616315_tsewwz.png"></div></div></div></div></div></div></div></div><span
+        class="spacer___2n7gY medium___1nzw6"></span><span
+        class="new-html-block___1_7NG"><h2 class="pad-header--s center--all" >Ansvars- og kaskoforsikring gjort helt enkelt</h2>
+<p style="text-align: center;" v-html="articleData.optional_extra_body"></p>
+</span><span
+        class="spacer___2n7gY medium___1nzw6"></span><div class="content___QPcta"><div
+        class="divider-wrapper___1QLQs main___3_Hz1"><div class="divider___3Bain"></div></div></div><span
+        class="new-html-block___1_7NG"></span>
+ <div class="insurance__container center--all owl--s" v-if="articleData">
+      <h2>Forsikringspriser</h2>
+      <div class="directus_table" v-html="articleData.body"></div>
+
+    </div>
+      <div
+        class="spots-list___2DWRP"><h2 class="title___2LvNh">Følgende er indeholdt i din forsikringspræmie</h2>
+        <h3 class="headline___3bXDI">Forsikringspræmie kort fortalt</h3>
+        <div
+          class="spots___1YBZo">
+          <div class="spot___3TRNI">
+            <div class="image___2eXCZ full-circle-image___1T9yg">
+              <img
+                src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_1_150x150_jctdcw.png"
+                width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">Stenslag</h4><div
+            class="markdown___35KUH description___W9AmU"><p>Gratis reparation af stenslag</p></div></div></div><div
+          class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+          src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_2_150x150_yhazyj.png"
+          width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">Føreulykkeforsikring</h4><div
+          class="markdown___35KUH description___W9AmU"><p>Gratis førerulykkesforsikring</p></div></div></div><div
+          class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+          src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_3_150x150_wsap05.png"
+          width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">Afleveringsforsikring</h4><div
+          class="markdown___35KUH description___W9AmU"><p>Gratis afleveringsforsikring selvrisiko kr. 7.500/5.000 for alle skader, dog maksimalt 37.500/25.000 totalt ved aflevering</p></div></div></div><div
+          class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+          src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_4_150x150_yumru6.png"
+          width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">Glasforsikring</h4><div
+          class="markdown___35KUH description___W9AmU"><p>Glasforsikring kan tilkøbes for kr. 59,- pr. måned og dækker skade på bilens glas, f.eks. spejl- og lygteglas eller ruder med en selvrisiko på kr. 1.000</p></div></div></div><div
+          class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+          src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_5_150x150_domzhx.png"
+          width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">Autohjælp</h4><div
+          class="markdown___35KUH description___W9AmU"><p>Autohjælp kan tilkøbes for kr. 49,- pr. måned</p></div></div></div><div
+          class="spot___3TRNI"><div class="image___2eXCZ full-circle-image___1T9yg"><img
+          src="https://res.cloudinary.com/digital-interdan-bilabonnement/image/upload/c_fit,q_80,w_256/v1/Fordele/Ikon_6_150x150_wo3aod.png"
+          width="%"></div><div class="text___2hHEU"><h4 class="title___1h-xf">PBS</h4><div
+          class="markdown___35KUH description___W9AmU"><p>PBS-gebyr kr. 10,- opkræves sammen med den månedlige forsikringspræmie</p></div></div></div></div></div><span
+          class="spacer___2n7gY small___RGjLY"></span><span class="spacer___2n7gY small___RGjLY"></span><div
+          class="content___QPcta"><div class="divider-wrapper___1QLQs large___1alyA main___3_Hz1"><div
+          class="divider___3Bain"></div></div></div></span></span>
+      <div v-if="articleData">
+<span
+  class="new-html-block___1_7NG"><h2 class="center--all pad-header--s">Vilkår og andet information</h2>
+  <p class="center--all">Når der indgås en aftale om obligatorisk forsikring gennem IF Forsikring, accepterer og samtykker leasingtager, at IF Forsikring må kontakte leasingtager med henblik på rådgivning om andre forsikringer. QUICK LEASING videregiver i den forbindelse leasingtagers CPR-nummer, telefonnummer, adresse samt mailadresse.</p>
+  <p class="center--all"></p>
+
+</span><span
+        class="spacer___2n7gY medium___1nzw6"></span>
+
+
+      </div></div>
+
+    <div class="section__gray">
+    <h2 class="center--all pad-header--m">Information om forsikringsformidling</h2>
+    <div class="container__cards">
+      <div class="card">
+
+        <div class="card-body">
+          <span class="tag tag-yellow">Kontakt</span>
+          <h4 class="pad-header--m">
+            Kontaktoplysninger på forsikringsselskabet er
+          </h4>
+          <p>IF Forsikring</p>
+          <p>Stamholmen 159</p>
+          <p>2650 Hvidovre</p>
+
+          <div class="user">
+
+            <div class="user-info">
+              <a href="https://quickleasing.dk/">
+              <button class="btn--primary">Hjemmeside</button></a>
+              <a href="tel:89885080">
+              <button class="btn--action bx bx-mobile">tlf: 89 88 50 80</button></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+
+        <div class="card-body">
+          <span class="tag tag-yellow">Kontakt</span>
+          <h4 class="pad-header--m">
+            Kontaktoplysninger på den accessoriske forsikringsformidler er
+          </h4>
+          <p>QUICK LEASING A/S</p>
+          <p>Slotsherrensvej 411C</p>
+          <p>2610 Rødovre</p>
+          <div class="user">
+            <div class="user-info">
+              <a href="https://www.if.dk/privat">
+                <button class="btn--primary">Hjemmeside</button></a>
+              <a href="tel:70121212">
+                <button class="btn--action bx bx-mobile">tlf: 70 12 12 12</button></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+
+        <div class="card-body">
+          <span class="tag tag-yellow">Klage</span>
+          <h4 class="pad-header--m">
+            Klagemuligheder
+          </h4>
+          <p>Såfremt du ønsker at klage over QUICK LEASING A/S som accessorisk forsikringsformidler, kan du sende en klage til</p>
+          <a href="mailto:support@quickleasing.dk">
+          <p>support@quickleasing.dk</p></a>
+
+          <div class="user">
+            <div class="user-info">
+              <a href="https://quickleasing.dk/">
+                <button class="btn--primary">Hjemmeside</button></a>
+              <a href="tel:89885080">
+                <button class="btn--action bx bx-mobile">tlf: 89 88 50 80</button></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+
+        <div class="card-body">
+          <span class="tag tag-yellow">Aflønning</span>
+          <h4 class="pad-header--m">
+            Økonomisk samarbejde med IF Forsikring
+          </h4>
+          <p>QUICK LEASING A/S har et økonomisk samarbejde med IF Forsikring og modtager provision ved formidling af
+            bilforsikringer. Medarbejdere ved QUICK LEASING modtager dog ingen provision eller anden direkte resultatafhængig løn ved formidling af bilforsikringer</p>
+
+          <div class="user">
+
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+
+        <div class="card-body">
+          <span class="tag tag-yellow">Garantiordning</span>
+          <h4 class="pad-header--m">
+            Beskyttet af garantifonden
+          </h4>
+          <p>Bilforsikring tegnet gennem IF Forsikring er skadesforsikring og dermed omfattet af garantifonden for skadesforsikringsselskaber.</p>
+
+          <div class="user">
+
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+
+        <div class="card-body">
+          <span class="tag tag-yellow">Register</span>
+          <h4 class="pad-header--m">
+            QUICK LEASING fungerer som accessorisk forsikringsformidler
+          </h4>
+          <p>QUICK LEASING fungerer som en formidler af accessorisk forsikring. Det betyder, at vi tilbyder forsikringer til vores kunder som en del af vores tjenester, men vi ikke selv udsteder forsikringerne. I stedet samarbejder vi med forsikringsselskaber og står for at formidle forsikringsaftalen mellem kunden og forsikringsselskabet.</p>
+          <p>Det gør det nemmere og mere praktisk for kunden at få den forsikring, de har brug for, da det kan være en uoverskuelig proces at undersøge og vælge den rigtige forsikring. QUICK LEASING hjælper kunden med at vælge den rette forsikring til deres behov og sørger for en glidende process fra start til slut.</p>
+
+          <div class="user">
+
+          </div>
+        </div>
+      </div>
+
+    </div>
     </div>
   </div>
   <footer-item />
@@ -46,6 +278,89 @@ export default {
     align-items: center;
   }
 }
+.insurance__container{
+  padding: var(--space-s) 0 var(--section-space-m);
+}
+.section__gray {
+  background-color: #F2F2F2;
+  padding: 50px 0;
+}
+.container__cards {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-content: center;
+}
+.card {
+  margin: 20px ;
+  border-radius: 10px;
+  background: #fff;
+  box-shadow: 0 2px 20px rgba(0, 0, 0, 0.2);
+  /* Create a gradient color where the left side is white and the right side is yellow */
+
+  overflow: hidden;
+  width: 50%;
+  /* Create spaceing between the rows */
+
+}
+/* Card responsive */
+@media (max-width: 768px) {
+  .card {
+    width: 85%;
+  }
+  .insurance__container{
+    padding: var(--space-s) var(--space-s);
+
+  }
+}
+
+.card-body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 20px;
+  min-height: 250px;
+}
+
+.tag {
+  background: #cccccc;
+  border-radius: 50px;
+  font-size: var(--text-s);
+  margin: 0;
+  color: black;
+  padding: 2px 10px;
+  text-transform: uppercase;
+  cursor: pointer;
+}
+
+.tag-yellow {
+  background-color: var(--secondary);
+}
+
+.card-body p {
+  font-size: var(--text-m);
+  margin: 0 0 var(--space-xs);
+}
+.user {
+  display: flex;
+  margin-top: auto;
+}
+
+.user img {
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+}
+.user-info h5 {
+  margin: 0;
+}
+.user-info small {
+  color: #545d7a;
+}
+
 
 /* Design a awesome table  */
 .directus_table table {
@@ -65,9 +380,6 @@ export default {
 .directus_table tr:nth-last-child(1) {
   font-weight: bold;
 }
-
-
-
 
 </style>
   
