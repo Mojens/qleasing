56ca9fe245aa6923bb52cd567d4c741a7fd857f5
mojens <moha135h@stud.kea.dk>
2023-02-10 16:41:04 +0100

    Man kan nu trykke mærker af og så ændre den de biler den viser

--- src/views/quickleasing.vue
+++ src/views/quickleasing.vue
@@ -1,31 +1,35 @@
 <template>
     <div class="ct-section-inner-wrap">
         <div v-if="carData">
-            <h1>{{ carData.brand + " " + carData.model }}</h1>
-            <br>
-            <div>
-                <h3>Mærke</h3>
-                <ul>
-                    <li v-for="brand in uniqueBrands" :key="brand">{{ brand }}</li>
-                </ul>
-            </div>
-            <div id="listeAfUdstyr">
-                <h2>Udstyr Overblik</h2>
-                <ul>
-                    <li v-for="item in items" :key="item">{{ item }}
-                        <input type="checkbox" :value="item" @click="handleCheckBoxClick(item.value)" />
-                    </li>
-                </ul>
-            </div>
+            <h1 v-for="car in carData" :key="car.id">{{ car.brand }} - {{ car.model }}</h1>
+        </div>
+
+        <br>
+        <div>
+            <h3>Mærke</h3>
+            <ul>
+                <li v-for="brand in uniqueBrands" :key="brand">
+                    {{ brand.name }} ({{ brand.count }})
+                    <input type="checkbox" :value="brand.name" :checked="selectedBrands.includes(brand.name)"
+                        @click="handleCheckboxClick(brand.name)" />
+
+                </li>
+            </ul>
         </div>
     </div>
 </template>
+  
 <script>
 export default {
     data() {
         return {
             name: "QuickLeasing",
-            carData: null,
+            carData: [],
+            selectedBrands: [],
+            originalData: [],
+            baseURL: import.meta.env.VITE_APP_CARS_URL,
+            currentURL: import.meta.env.VITE_APP_CARS_URL,
+            readerAPI: import.meta.env.VITE_APP_READER_API
         };
     },
     async created() {
@@ -33,22 +37,68 @@ export default {
     },
     methods: {
         async fetchData() {
+            console.log("DEBUG 1: " + this.carData);
             const response = await fetch(import.meta.env.VITE_APP_CARS_URL, {
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json",
-                    Authorization: `Bearer ${import.meta.env.VITE_APP_READER_API}`
+                    Authorization: `Bearer ${this.readerAPI}`
                 }
             });
             const data = await response.json();
             this.carData = data.data;
-            this.items = this.carData.udstyr;
+            this.originalData = data.data;
+        },
+        async handleCheckboxClick(value) {
+            console.log("DEBUG 2: " + this.carData);
+            let filter = { brand: { _in: [value] } };
+
+            if (this.selectedBrands.includes(value)) {
+                this.selectedBrands = this.selectedBrands.filter(brand => brand !== value);
+            } else {
+                this.selectedBrands.push(value);
+            }
+
+            filter = { brand: { _in: this.selectedBrands } };
+
+            this.currentURL = `${this.baseURL}?filter=${encodeURIComponent(JSON.stringify(filter))}`;
+            console.log("DEBUG "+this.selectedBrands.length)
+            if (this.selectedBrands.length === 0) {
+                this.currentURL = this.baseURL;
+            }
+
+            const checkboxResponse = await fetch(this.currentURL, {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${this.readerAPI}`
+                }
+            });
+            const data = await checkboxResponse.json();
+            this.carData = data.data;
         }
+
+
 
     },
     computed: {
         uniqueBrands() {
-            return [...new Set(this.carData.map(car => car.brand))]
+            console.log("DEBUG 3: " + this.carData);
+            const brandCount = {};
+            this.originalData.forEach(car => {
+                if (!brandCount[car.brand]) {
+                    brandCount[car.brand] = 1;
+                } else {
+                    brandCount[car.brand]++;
+                }
+            });
+
+            return Object.entries(brandCount).map(([name, count]) => ({
+                name,
+                count
+
+
+            }));
         }
     }
 
