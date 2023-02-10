f2989c1e4e61f90633a1688faed9d574a5522c38
mojens <moha135h@stud.kea.dk>
2023-02-10 15:24:17 +0100

    distinct list of brands

--- src/views/quickleasing.vue
+++ src/views/quickleasing.vue
@@ -3,11 +3,18 @@
         <div v-if="carData">
             <h1>{{ carData.brand + " " + carData.model }}</h1>
             <br>
+            <div>
+                <h3>Mærke</h3>
+                <ul>
+                    <li v-for="brand in uniqueBrands" :key="brand">{{ brand }}</li>
+                </ul>
+            </div>
             <div id="listeAfUdstyr">
                 <h2>Udstyr Overblik</h2>
                 <ul>
                     <li v-for="item in items" :key="item">{{ item }}
-                        <input type="checkbox" :value="item" @click="handleCheckBoxClick(item.value)"/></li>
+                        <input type="checkbox" :value="item" @click="handleCheckBoxClick(item.value)" />
+                    </li>
                 </ul>
             </div>
         </div>
@@ -19,32 +26,12 @@ export default {
         return {
             name: "QuickLeasing",
             carData: null,
-            props: {
-                items: {
-                    type: Array,
-                }
-            }
         };
     },
     async created() {
         await this.fetchData();
     },
     methods: {
-        async handleCheckBoxClick(value) {
-            const response = await fetch(import.meta.env.VITE_APP_CARS_URL+'?search='+value, {
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${import.meta.env.VITE_APP_READER_API}`
-                }
-            });
-            const data = await response.json();
-            this.carData = data.data[0];
-            this.items = this.carData
-            if (this.items[0] === "") {
-                this.items.shift();
-            }
-        },
         async fetchData() {
             const response = await fetch(import.meta.env.VITE_APP_CARS_URL, {
                 headers: {
@@ -54,13 +41,15 @@ export default {
                 }
             });
             const data = await response.json();
-            this.carData = data.data[0];
+            this.carData = data.data;
             this.items = this.carData.udstyr;
-            if (this.items[0] === "") {
-                this.items.shift();
-            }
         }
 
+    },
+    computed: {
+        uniqueBrands() {
+            return [...new Set(this.carData.map(car => car.brand))]
+        }
     }
 
 };
