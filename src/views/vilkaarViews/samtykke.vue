17a2438ba9171832c4ea878e470f1443c0800c39
IfMalthereturnawesome <malt996b@stud.kea.dk>
2023-02-11 22:19:13 +0100

    Deleted Pictures for vilkår og politik pages

--- src/views/vilkaarViews/samtykke.vue
+++ src/views/vilkaarViews/samtykke.vue
@@ -3,53 +3,19 @@ import FooterItem from "../../components/FooterItem.vue";
 </script>
 
 <template>
-  <div>
-    <div
-      id="section-0-row-0"
-      class="grid__row grid__theme section grid__theme--dark grid__theme--middle grid__theme--is-first grid__theme--top-padding ng-star-inserted">
-      <div class="container">
-        <div id="spot-0000" class="ng-star-inserted" genericspot=""></div>
-        <div class="container is-full animate-load ng-star-inserted" ncgloadin="">
-          <div class="container is-fullwidth">
-            <div class="hero-spot"><!---->
-              <picture class="hero-spot__media-container ng-star-inserted">
-                <source media="(min-width: 1024px)"
-                        srcSet="https://admin.hyundai.dk/media/n50fxf4r/provekor.jpg?width=1550&amp;heightratio=0.4285703125&amp;mode=crop 1x, https://admin.hyundai.dk/media/n50fxf4r/provekor.jpg?width=3100&amp;heightratio=0.4285703125&amp;mode=crop 2x">
-                <source media="(min-width: 768px) and (max-width: 1023px)"
-                        srcSet="https://admin.hyundai.dk/media/n50fxf4r/provekor.jpg?width=1024&amp;heightratio=0.4285703125&amp;mode=crop 1x, https://admin.hyundai.dk/media/n50fxf4r/provekor.jpg?width=2048&amp;heightratio=0.4285703125&amp;mode=crop 2x">
-                <source media="(min-width: 414px) and (max-width: 767px)"
-                        srcSet="https://admin.hyundai.dk/media/n50fxf4r/provekor.jpg?width=767&amp;heightratio=1.2487309645&amp;mode=crop 1x, https://admin.hyundai.dk/media/n50fxf4r/provekor.jpg?width=1534&amp;heightratio=1.2487309645&amp;mode=crop 2x">
-                <source media="(max-width: 413px)"
-                        srcSet="https://admin.hyundai.dk/media/n50fxf4r/provekor.jpg?width=394&amp;heightratio=1.2487309645&amp;mode=crop 1x, https://admin.hyundai.dk/media/n50fxf4r/provekor.jpg?width=788&amp;heightratio=1.2487309645&amp;mode=crop 2x">
-                <img
-                  alt="Afhængertræk af bilen er en god ide "
-                  class="ng-star-inserted"
-                  src="https://admin.hyundai.dk/media/n50fxf4r/provekor.jpg?width=1550&amp;heightratio=0.4285703125&amp;mode=crop">
-                <!----><!----></picture>
-              <!----><!----><!---->
-              <div class="hero-spot__content animate-load ng-star-inserted" ncgloadin="">
-                <div class="spot-content">
-                  <header class="spot-content__header ng-star-inserted"><!----><h1
-                    class="spot-content__header--title ng-star-inserted"><span
-                    style="font-weight: 700;">{{ articleData.title }}</span></h1>
-                    <!----><!----><!----><!----><!----></header><!----></div>
-                <!----></div><!----><!----></div>
-          </div>
-        </div><!----><!----><!----><!----></div>
-      <!----></div><!---->
-
+  <div class="ct-section-inner-wrap">
     <div v-if="articleData">
-      <span
-        class="new-html-block___3_7NG">
+      <span class="new-html-block___3_7NG">
+        <h1 class="pad-header--m">{{ articleData.title }}</h1>
+        <p v-html="articleData.body"></p>
 
-  <p v-html="articleData.body"></p>
-       <RouterLink class="buttons___3URuA" to="/quickleasing"><button
-         class="button-primary">Find din næste quickleasing bil</button></RouterLink>
+        <RouterLink class="buttons___3URuA" to="/quickleasing"
+          ><button class="button-primary">
+            Find din næste quickleasing bil
+          </button></RouterLink
+        >
       </span>
-
-
     </div>
-
   </div>
   <footer-item />
 </template>
@@ -57,7 +23,7 @@ import FooterItem from "../../components/FooterItem.vue";
 export default {
   data() {
     return {
-      articleData: null
+      articleData: null,
     };
   },
   async created() {
@@ -71,16 +37,14 @@ export default {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          Authorization: `Bearer ${READER_API}`
-        }
+          Authorization: `Bearer ${READER_API}`,
+        },
       });
       const data = await response.json();
       this.articleData = data.data;
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
