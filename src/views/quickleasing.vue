a90dba43f3150f9cacddbbc685288c3e884f99b8
mojens <moha135h@stud.kea.dk>
2023-02-11 15:51:54 +0100

    filtre udstyr i javascript

--- src/views/quickleasing.vue
+++ src/views/quickleasing.vue
@@ -12,59 +12,16 @@
                     <input type="checkbox" :value="brand.name" :checked="selectedBrands.includes(brand.name)"
                         @click="handleCheckboxClick(brand.name)" />
                     {{ brand.name }} ({{ brand.count }})
-
-
                 </li>
             </ul>
         </div>
         <br>
         <div>
             <h3>Udstyr</h3>
-            <ul id="udstyrListe2">
-                <li value="airc">Air Condition
-                    <input type="checkbox" />
-                </li>
-                <li value="fartpilot">Fartpilot
-                    <input type="checkbox" />
-                </li>
-                <li value="bluetooth">Bluetooth
-                    <input type="checkbox" />
-                </li>
-                <li value="sædevarme">Sædevarme
-                    <input type="checkbox" />
-                </li>
-                <li value="varme i rat">Rat-varme
-                    <input type="checkbox" />
-                </li>
-                <li value="parkeringssensor (for)">Parkeringssensor (foran)
-                    <input type="checkbox" />
-                </li>
-                <li value="parkeringssensor (bag)">Parkeringssensor (bag)
-                    <input type="checkbox" />
-                </li>
-                <li value="navigation">Navigation
-                    <input type="checkbox" />
-                </li>
-                <li value="aut.gear/tiptronic">Automatgear
-                    <input type="checkbox" />
-                </li>
-                <li value="Anhængertræk">Anhængertræk
-                    <input type="checkbox" />
-                </li>
-                <li value="4x el-ruder">4 elruder
-                    <input type="checkbox" />
-                </li>
-                <li value="5 personers">5 personers
-                    <input type="checkbox" />
-                </li>
-                <li value="bakkamera">Bakkamera
-                    <input type="checkbox" />
-                </li>
-                <li value="Apple CarPlay">Apple CarPlay
-                    <input type="checkbox" />
-                </li>
-                <li value="Android Auto">Android Auto
-                    <input type="checkbox" />
+            <ul>
+                <li v-for="item in featureItems" :key="item.value" :data-value="item.value">
+                    <input type="checkbox" @click="handleCheckboxClickFeatures(item.value)" />
+                    {{ item.name }}
                 </li>
             </ul>
         </div>
@@ -82,6 +39,24 @@ export default {
             baseURL: import.meta.env.VITE_APP_CARS_URL,
             currentURL: import.meta.env.VITE_APP_CARS_URL,
             readerAPI: import.meta.env.VITE_APP_READER_API,
+            checkBoxState: {},
+            featureItems: [
+                { value: "airc", name: "Air Condition" },
+                { value: "fartpilot", name: "Fartpilot" },
+                { value: "bluetooth", name: "Bluetooth" },
+                { value: "sædevarme", name: "Sædevarme" },
+                { value: "varmeirat", name: "Rat-varme" },
+                { value: "parkeringssensorfor", name: "Parkeringssensor (foran)" },
+                { value: "parkeringssensorbag", name: "Parkeringssensor (bag)" },
+                { value: "navigation", name: "Navigation" },
+                { value: "autgeartiptronic", name: "Automatgear" },
+                { value: "Anhaengertraek", name: "Anhængertræk" },
+                { value: "4xelruder", name: "4 elruder" },
+                { value: "5personers", name: "5 personers" },
+                { value: "bakkamera", name: "Bakkamera" },
+                { value: "applecarplay", name: "Apple CarPlay" },
+                { value: "androidauto", name: "Android Auto" },
+            ]
         };
     },
     async created() {
@@ -89,7 +64,6 @@ export default {
     },
     methods: {
         async fetchData() {
-            console.log("DEBUG 1: " + this.carData);
             const response = await fetch(import.meta.env.VITE_APP_CARS_URL, {
                 headers: {
                     Accept: "application/json",
@@ -102,7 +76,6 @@ export default {
             this.originalData = data.data;
         },
         async handleCheckboxClick(value) {
-            console.log("DEBUG 2: " + this.carData);
             let filter = { brand: { _in: [value] } };
 
             if (this.selectedBrands.includes(value)) {
@@ -114,7 +87,6 @@ export default {
             filter = { brand: { _in: this.selectedBrands } };
 
             this.currentURL = `${this.baseURL}?filter=${encodeURIComponent(JSON.stringify(filter))}`;
-            console.log("DEBUG " + this.selectedBrands.length)
             if (this.selectedBrands.length === 0) {
                 this.currentURL = this.baseURL;
             }
@@ -128,11 +100,46 @@ export default {
             });
             const data = await checkboxResponse.json();
             this.carData = data.data;
+        },
+        async handleCheckboxClickFeatures(value) {
+            const response = await fetch(this.currentURL, {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${this.readerAPI}`
+                }
+            });
+            const data = await response.json();
+            const allData = data.data;
+            
+            this.checkBoxState[value] = !this.checkBoxState[value];
+            if (!this.checkBoxState[value]) {
+                if (this.selectedBrands.length === 0) {
+                    this.carData = this.originalData;
+                }
+                this.carData = allData;
+                return;
+            }
+            
+
+            const filteredCars = [];
+            for (let i = 0; i < allData.length; i++) {
+                if (allData[i].Udstyr !== null) {
+                    console.log("udstyr")
+                    console.log(allData[i].Udstyr[0])
+                    for (let x = 0; x < allData[i].Udstyr.length; x++) {
+                        if (allData[i].Udstyr[x] === value) {
+                            filteredCars.push(allData[i]);
+                        }
+                    }
+                }
+            }
+            this.carData = filteredCars;
         }
+
     },
     computed: {
         uniqueBrands() {
-            console.log("DEBUG 3: " + this.carData);
             const brandCount = {};
             this.originalData.forEach(car => {
                 if (!brandCount[car.brand]) {
