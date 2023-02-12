5543d789e86729262432bee5cd4130af702da584
mojens <moha135h@stud.kea.dk>
2023-02-12 23:10:58 +0100

    filter query

--- src/views/quickleasing.vue
+++ src/views/quickleasing.vue
@@ -102,9 +102,26 @@ export default {
     },
     methods: {
         async fetchData() {
-            if (this.queryBrand !== '' && this.queryBrand !== '' && this.queryPrice !== '') {
-                console.log('brand', this.queryBrand);
-            }
+            if (this.queryBrand.length > 1 && this.queryBrand.length > 1 && this.queryPrice.length > 1) {
+                const response = await fetch(this.currentURL, {
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json",
+                        Authorization: `Bearer ${this.readerAPI}`
+                    }
+                });
+                const data = await response.json();
+                this.carData = data.data;
+                this.originalData = data.data;
+                for (let i = 0; i < this.carData.length; i++) {
+                    if (this.carData[i].brand === this.queryBrand && this.carData[i].model === this.queryModel) {
+                        this.carData = [this.carData[i]];
+                    }
+                    if (this.carData[i].brand === this.queryBrand && this.carData[i].model === this.queryModel && this.carData[i].base_maanedspris <! this.queryPrice || this.carData[i].base_maanedspris >! this.queryPrice) {
+                        this.carData = [this.carData[i]];
+                    }
+                }
+            } else { 
             const response = await fetch(this.currentURL, {
                 headers: {
                     Accept: "application/json",
@@ -115,155 +132,156 @@ export default {
             const data = await response.json();
             this.carData = data.data;
             this.originalData = data.data;
-        },
-        async handleCheckboxClick(value) {
-            let filter = { brand: { _in: [value] } };
-
-            if (this.selectedBrands.includes(value)) {
-                this.selectedBrands = this.selectedBrands.filter(brand => brand !== value);
-            } else {
-                this.selectedBrands.push(value);
-            }
-            filter = { brand: { _in: this.selectedBrands } };
-
-            this.currentURL = `${this.baseURL}?filter=${encodeURIComponent(JSON.stringify(filter))}`;
-
-            if (this.selectedBrands.length === 0) {
-                this.currentURL = this.baseURL;
-            }
-            const checkboxResponse = await fetch(this.currentURL, {
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${this.readerAPI}`
-                }
-            });
-            const data = await checkboxResponse.json();
-            this.carData = data.data;
         }
-
-        ,
-        async handleCheckboxClickFeatures(value) {
-            const response = await fetch(this.currentURL, {
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${this.readerAPI}`
-                }
-            });
-            const data = await response.json();
-            const allData = data.data;
-
-            this.checkBoxState[value] = !this.checkBoxState[value];
-
-            if (this.checkBoxState[value]) {
-                this.selectedFeatures.push(value);
-            } else {
-                this.selectedFeatures = this.selectedFeatures.filter(f => f !== value);
-            }
-
-            if (this.selectedFeatures.length === 0) {
-                this.carData = allData;
-                return;
-            }
-            const filteredCars = [];
-            for (let i = 0; i < allData.length; i++) {
-                if (allData[i].Udstyr !== null) {
-                    let match = true;
-                    for (let x = 0; x < this.selectedFeatures.length; x++) {
-                        if (!allData[i].Udstyr.includes(this.selectedFeatures[x])) {
-                            match = false;
-                            break;
-                        }
-                    }
-                    if (match) {
-                        filteredCars.push(allData[i]);
-                    }
-                }
-            }
-
-            this.carData = Array.from(filteredCars);
-        }
-        ,
-        async updateFeatureCounts(cars) {
-            cars.forEach(car => {
-                if (car.Udstyr !== null) {
-                    car.Udstyr.forEach(feature => {
-                        const foundFeature = this.featureItems.find(
-                            item => item.value === feature
-                        );
-                        if (foundFeature) {
-                            foundFeature.count++;
-                        }
-                    });
-                }
-            });
-        },
-        async priceChange() {
-            const response = await fetch(this.currentURL, {
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${this.readerAPI}`
-                }
-            });
-            const data = await response.json();
-            const allData = data.data;
-            if (this.selectedPrice === '*') {
-                this.carData = data.data
-                return;
-            }
-            const priceRange1000_2000 = [];
-            const priceRange2000_3000 = [];
-            const priceRange3000_4000 = [];
-            const priceRange4000_5000 = [];
-            const priceRange5000_plus = [];
-            for (let i = 0; i < allData.length; i++) {
-                if (allData[i].base_maanedspris > 999 && allData[i].base_maanedspris <= 2000) {
-                    priceRange1000_2000.push(allData[i]);
-                } else if (allData[i].base_maanedspris > 1999 && allData[i].base_maanedspris <= 3000) {
-                    priceRange2000_3000.push(allData[i]);
-                } else if (allData[i].base_maanedspris > 2999 && allData[i].base_maanedspris <= 4000) {
-                    priceRange3000_4000.push(allData[i]);
-                } else if (allData[i].base_maanedspris > 3999 && allData[i].base_maanedspris <= 5000) {
-                    priceRange4000_5000.push(allData[i]);
-                } else {
-                    priceRange5000_plus.push(allData[i]);
-                }
-            }
-            if (this.selectedPrice === '1000-2000') {
-                this.carData = priceRange1000_2000;
-            } else if (this.selectedPrice === '2000-3000') {
-                this.carData = priceRange2000_3000;
-            } else if (this.selectedPrice === '3000-4000') {
-                this.carData = priceRange3000_4000;
-            } else if (this.selectedPrice === '4000-5000') {
-                this.carData = priceRange4000_5000;
-            } else {
-                this.carData = priceRange5000_plus;
-            }
-        }
-
-
     },
-    computed: {
-        uniqueBrands() {
-            const brandCount = {};
-            this.originalData.forEach(car => {
-                if (!brandCount[car.brand]) {
-                    brandCount[car.brand] = 1;
-                } else {
-                    brandCount[car.brand]++;
+    async handleCheckboxClick(value) {
+        let filter = { brand: { _in: [value] } };
+
+        if (this.selectedBrands.includes(value)) {
+            this.selectedBrands = this.selectedBrands.filter(brand => brand !== value);
+        } else {
+            this.selectedBrands.push(value);
+        }
+        filter = { brand: { _in: this.selectedBrands } };
+
+        this.currentURL = `${this.baseURL}?filter=${encodeURIComponent(JSON.stringify(filter))}`;
+
+        if (this.selectedBrands.length === 0) {
+            this.currentURL = this.baseURL;
+        }
+        const checkboxResponse = await fetch(this.currentURL, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${this.readerAPI}`
+            }
+        });
+        const data = await checkboxResponse.json();
+        this.carData = data.data;
+    }
+
+    ,
+    async handleCheckboxClickFeatures(value) {
+        const response = await fetch(this.currentURL, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${this.readerAPI}`
+            }
+        });
+        const data = await response.json();
+        const allData = data.data;
+
+        this.checkBoxState[value] = !this.checkBoxState[value];
+
+        if (this.checkBoxState[value]) {
+            this.selectedFeatures.push(value);
+        } else {
+            this.selectedFeatures = this.selectedFeatures.filter(f => f !== value);
+        }
+
+        if (this.selectedFeatures.length === 0) {
+            this.carData = allData;
+            return;
+        }
+        const filteredCars = [];
+        for (let i = 0; i < allData.length; i++) {
+            if (allData[i].Udstyr !== null) {
+                let match = true;
+                for (let x = 0; x < this.selectedFeatures.length; x++) {
+                    if (!allData[i].Udstyr.includes(this.selectedFeatures[x])) {
+                        match = false;
+                        break;
+                    }
                 }
-            });
+                if (match) {
+                    filteredCars.push(allData[i]);
+                }
+            }
+        }
 
-            return Object.entries(brandCount).map(([name, count]) => ({
-                name,
-                count
-
-            }));
+        this.carData = Array.from(filteredCars);
+    }
+    ,
+    async updateFeatureCounts(cars) {
+        cars.forEach(car => {
+            if (car.Udstyr !== null) {
+                car.Udstyr.forEach(feature => {
+                    const foundFeature = this.featureItems.find(
+                        item => item.value === feature
+                    );
+                    if (foundFeature) {
+                        foundFeature.count++;
+                    }
+                });
+            }
+        });
+    },
+    async priceChange() {
+        const response = await fetch(this.currentURL, {
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${this.readerAPI}`
+            }
+        });
+        const data = await response.json();
+        const allData = data.data;
+        if (this.selectedPrice === '*') {
+            this.carData = data.data
+            return;
+        }
+        const priceRange1000_2000 = [];
+        const priceRange2000_3000 = [];
+        const priceRange3000_4000 = [];
+        const priceRange4000_5000 = [];
+        const priceRange5000_plus = [];
+        for (let i = 0; i < allData.length; i++) {
+            if (allData[i].base_maanedspris > 999 && allData[i].base_maanedspris <= 2000) {
+                priceRange1000_2000.push(allData[i]);
+            } else if (allData[i].base_maanedspris > 1999 && allData[i].base_maanedspris <= 3000) {
+                priceRange2000_3000.push(allData[i]);
+            } else if (allData[i].base_maanedspris > 2999 && allData[i].base_maanedspris <= 4000) {
+                priceRange3000_4000.push(allData[i]);
+            } else if (allData[i].base_maanedspris > 3999 && allData[i].base_maanedspris <= 5000) {
+                priceRange4000_5000.push(allData[i]);
+            } else {
+                priceRange5000_plus.push(allData[i]);
+            }
+        }
+        if (this.selectedPrice === '1000-2000') {
+            this.carData = priceRange1000_2000;
+        } else if (this.selectedPrice === '2000-3000') {
+            this.carData = priceRange2000_3000;
+        } else if (this.selectedPrice === '3000-4000') {
+            this.carData = priceRange3000_4000;
+        } else if (this.selectedPrice === '4000-5000') {
+            this.carData = priceRange4000_5000;
+        } else {
+            this.carData = priceRange5000_plus;
         }
     }
+
+
+},
+computed: {
+    uniqueBrands() {
+        const brandCount = {};
+        this.originalData.forEach(car => {
+            if (!brandCount[car.brand]) {
+                brandCount[car.brand] = 1;
+            } else {
+                brandCount[car.brand]++;
+            }
+        });
+
+        return Object.entries(brandCount).map(([name, count]) => ({
+            name,
+            count
+
+        }));
+    }
+}
 
 };
 </script>
