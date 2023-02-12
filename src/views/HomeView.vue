fc58d96ae38ea7e1a7268146ef575656b1900f26
mojens <moha135h@stud.kea.dk>
2023-02-12 22:20:27 +0100

    brand og model

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,15 +22,19 @@
             </select>
           </td>
           <td>
-            <select>
+            <select v-model="selectedBrand">
               <option value="*">Alle</option>
-              <option v-for="brand in uniqueBrands" :key="brand.id" :value="brand.name">{{ brand.name }} ({{ brand.count }})</option>
+              <option v-for="brand in uniqueBrands" :key="brand.id" :value="brand.name">{{ brand.name }} ({{
+                brand.count
+              }})</option>
             </select>
           </td>
           <td>
-            <select>
+            <select v-model="selectedModel">
               <option value="*">Alle</option>
-              <option v-for="model in modelForBrand" :key="model.id" :value="model.name">{{ model.name }} ({{ model.count }})</option>
+              <option v-for="model in filteredModels" :key="model.id" :value="model.name">{{ model.name }} ({{
+                model.count
+              }})</option>
             </select>
           </td>
           <td>
@@ -53,7 +57,9 @@ export default {
       currentURL: import.meta.env.VITE_APP_CARS_URL,
       pictureURL: import.meta.env.VITE_APP_PICTURE_URL,
       readerAPI: import.meta.env.VITE_APP_READER_API,
-      selectedPrice: "*"
+      selectedPrice: "*",
+      selectedBrand: "*",
+      selectedModel: "*",
     };
   },
   async created() {
@@ -75,39 +81,54 @@ export default {
 
   },
   computed: {
-        uniqueBrands() {
-            const brandCount = {};
-            this.carData.forEach(car => {
-                if (!brandCount[car.brand]) {
-                    brandCount[car.brand] = 1;
-                } else {
-                    brandCount[car.brand]++;
-                }
-            });
-
-            return Object.entries(brandCount).map(([name, count]) => ({
-                name,
-                count
-
-            }));
-        },
-         modelForBrand() {
-            const modelCount = {};
-            this.carData.forEach(car => {
-                if (!modelCount[car.model]) {
-                    modelCount[car.model] = 1;
-                } else {
-                    modelCount[car.model]++;
-                }
-            });
-
-            return Object.entries(modelCount).map(([name, count]) => ({
-                name,
-                count
-
-            }));
+    uniqueBrands() {
+      const brandCount = {};
+      this.carData.forEach(car => {
+        if (!brandCount[car.brand]) {
+          brandCount[car.brand] = 1;
+        } else {
+          brandCount[car.brand]++;
         }
+      });
+
+      return Object.entries(brandCount).map(([name, count]) => ({
+        name,
+        count
+
+      }));
+    },
+    modelForBrand() {
+      const brandFilteredData = this.carData.filter(car => {
+        return this.selectedBrand === '*' || car.brand === this.selectedBrand;
+      });
+
+      const modelCount = {};
+      brandFilteredData.forEach(car => {
+        if (!modelCount[car.model]) {
+          modelCount[car.model] = 1;
+        } else {
+          modelCount[car.model]++;
+        }
+      });
+
+      return Object.entries(modelCount).map(([name, count]) => ({
+        name,
+        count
+      }));
+    },
+    filteredModels() {
+      if (this.selectedBrand === '*') {
+        return this.modelForBrand;
+      }
+
+      return this.modelForBrand.filter(model => {
+        return this.carData.some(car => {
+          return car.brand === this.selectedBrand && car.model === model.name;
+        });
+      });
     }
+
+  }
 
 };
 </script>
