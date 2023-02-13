2c9238637d80078fa22b8f7f010f94602d7a1acb
mojens <moha135h@stud.kea.dk>
2023-02-13 22:35:28 +0100

    fået v-select til at virke

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
 
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
-
 const app = createApp(App);
+app.component("v-select", vSelect);
 
 app.use(createPinia());
 app.use(router);
